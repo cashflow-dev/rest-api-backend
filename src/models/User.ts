df219fdebff2537f08285523c97c4ee5f05e534e
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-06-26 19:27:13 +0200

    Add Use Model (Close #1)

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface User extends Document {
+  email: string;
+  password: string;
+  settings: Settings;
+}
+
+interface Settings extends Document {
+  currency: string;
+}
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  settings: {
+    currency: String,
+  },
+});
+
+export default model<User>('User', userSchema);
