b258b216c98f2dfb2d97206d21ce2c3b1bbb773a
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-06-27 07:18:07 +0200

    Add routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,11 @@
 import Router from 'koa-router';
+import * as userController from '../controllers/user';
 
 const router = new Router({ prefix: '/user' });
 
-router.get(
-  '/',
-  async (ctx, next): Promise<void> => {
-    ctx.body = 'Some User';
-    next();
-  },
-);
+router.get('/:id', userController.findById);
+router.post('/', userController.create);
+router.patch('/:id', userController.update);
+router.get('/', userController.find);
 
 export default router;
