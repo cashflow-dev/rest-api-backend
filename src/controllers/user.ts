83603ed1b584eca8603fca8bda33f7082cf78c6a
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-06-27 22:25:21 +0200

    Change to use launcher

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,20 +1,23 @@
 import { ParameterizedContext } from 'koa';
-import { UserService } from '../services/UserService';
-import { serviceLaunch } from '../services';
+import { UserService , serviceLaunch } from '../services';
+
 
 export const create = async (context: ParameterizedContext): Promise<void> => {
-  const userService = serviceLaunch(UserService, context);
-  userService.create();
+  serviceLaunch(UserService, 'create', context);
 };
+
 export const find = async (context: ParameterizedContext): Promise<void> => {
-  const userService = serviceLaunch(UserService, context);
-  userService.find();
+  serviceLaunch(UserService, 'find', context);
 };
+
 export const findById = async (context: ParameterizedContext): Promise<void> => {
-  const userService = serviceLaunch(UserService, context);
-  userService.findById();
+  serviceLaunch(UserService, 'findById', context);
 };
+
 export const update = async (context: ParameterizedContext): Promise<void> => {
-  const userService = serviceLaunch(UserService, context);
-  userService.update();
+  serviceLaunch(UserService, 'update', context);
+};
+
+export const deleteById = async (context: ParameterizedContext): Promise<void> => {
+  serviceLaunch(UserService, 'deleteById', context);
 };
