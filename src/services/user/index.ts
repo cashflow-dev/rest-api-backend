2f6508e72867871ba26ae8e1a0b00fa162857122
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-07-21 17:47:35 +0200

    Create ServiceBase class (Close #35)

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,106 +1,15 @@
-import UserModel, { User } from '../../models/User';
+import UserModel from '../../models/User';
 import { InputData } from '../../interfaces/InputData';
-import { Service } from '../../interfaces/Service';
-import { composeValidators, validateId, validateQuery } from '../../libs/validators';
+import { composeValidators, validateId } from '../../libs/validators';
 import { validateCreateBody, validateUpdateBody } from './validators';
-import NotFoundError from '../../helpers/NotFoundError';
-import { getRequestedFields, handleMongoErrors } from '../../helpers';
+import ServiceBase from '../../libs/ServiceBase';
 
-export class UserService implements Service {
-  public input: InputData;
-
-  public DEFAULT_LIMIT: number;
-
-  public Model: any;
-
-  public requestedFields: any;
-
-  public queryFilter: any;
-
-  public validators: any;
-
+export class UserService extends ServiceBase {
   public constructor(input: InputData) {
-    this.DEFAULT_LIMIT = 50;
+    super(input);
     this.Model = UserModel;
-    this.input = input;
-    this.validators = {
-      find: composeValidators(validateQuery),
-      findById: composeValidators(validateId),
-      update: composeValidators(validateId, validateUpdateBody),
-      create: composeValidators(validateCreateBody),
-      delete: composeValidators(validateId),
-    };
-
-    /* eslint-disable no-underscore-dangle */
-    this.requestedFields = this.input.fields ? getRequestedFields(this.input.fields) : { _id: 1 };
-    this.queryFilter = this.input.next
-      ? {
-          _id: { $lt: this.input.next },
-        }
-      : {};
-  }
-
-  public async findById(): Promise<User | undefined> {
-    let result;
-    try {
-      result = await this.Model.findById(this.input.params.id).select(this.requestedFields);
-    } catch (e) {
-      handleMongoErrors(e);
-    }
-    if (result === null) {
-      throw new NotFoundError();
-    }
-    return result;
-  }
-
-  public async create(): Promise<User | undefined> {
-    let result;
-    try {
-      const model = new this.Model(this.input.body);
-      result = await model.save();
-    } catch (e) {
-      handleMongoErrors(e);
-    }
-    return result;
-  }
-
-  public async find(): Promise<any> {
-    let results: any;
-    try {
-      results = await this.Model.find(this.queryFilter)
-        .select(this.requestedFields)
-        .sort({ _id: -1 })
-        .limit(Number(this.input.limit) || this.DEFAULT_LIMIT);
-    } catch (e) {
-      handleMongoErrors(e);
-    }
-    return { users: results, next: results[results.length - 1]._id };
-  }
-
-  public async update(): Promise<User | null> {
-    let result = null;
-    try {
-      result = await this.Model.findByIdAndUpdate(this.input.params.id, this.input.body);
-    } catch (e) {
-      handleMongoErrors(e);
-    }
-    if (result === null) {
-      throw new NotFoundError();
-    }
-    return result;
-  }
-
-  public async delete(): Promise<User | null> {
-    let result = null;
-    try {
-      result = await this.Model.findByIdAndDelete(this.input.params.id);
-    } catch (e) {
-      handleMongoErrors(e);
-    }
-    if (result === null) {
-      throw new NotFoundError();
-    }
-    return result;
+    this.validators.update = composeValidators(validateId, validateUpdateBody);
+    this.validators.create = composeValidators(validateCreateBody);
   }
 }
 
