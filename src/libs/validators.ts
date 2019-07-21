d58c7185744c030ca1fb291c985cd380db2b59a9
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-07-21 16:35:20 +0200

    Remove old functions (Close #25)

--- src/libs/validators.ts
+++ src/libs/validators.ts
@@ -6,28 +6,6 @@ import ValidationError from '../helpers/ValidationError';
 
 const ObjectId = joiObjectId(Joi);
 
-/* eslint-disable no-param-reassign */
-/* const stripNonEqual = (source: any, dest: any) => {
-  const sourceEntries = Object.entries(source);
-  const destKeys: any = Object.entries(dest);
-
-  sourceEntries.forEach(([key, value]: any[], i: number) => {
-    const isObject = typeof value === 'object' && value !== null && !Array.isArray(value);
-
-    if (isObject) {
-      stripNonEqual(source[key], dest[destKeys[i]]);
-    }
-    if (destKeys.includes(key)) {
-      delete source[key];
-    }
-  });
-};
-
-const stripNotValidated = (schemas: object[], inputData: InputData) => {
-  const fullSchema = schemas.reduce((a, b) => _.merge(a, b));
-  return stripNonEqual(inputData, fullSchema);
-}; */
-
 export const composeValidators = (...functions: ((inputData: InputData) => void)[]) => {
   return function(inputData: InputData) {
     functions.forEach(func => {
