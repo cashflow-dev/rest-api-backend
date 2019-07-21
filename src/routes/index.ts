1f2a894057f0cfb05a327ea55d311be5ca1ce1d0
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-07-21 16:37:25 +0200

    Remove WIP routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import combineRouters from 'koa-combine-routers';
 import userRouter from './user';
-import accountRouter from './account';
-import rootRouter from './root';
 
-const router = combineRouters(accountRouter, userRouter, rootRouter);
+const router = combineRouters(userRouter);
 
 export default router;
--- src/routes/root.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-import Router from 'koa-router';
-
-const router = new Router({ prefix: '/root' });
-
-router.get('/', async (context: any) => {
-  context.body = 'Hello world';
-});
-export default router;
