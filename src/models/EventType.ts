985cb0542b8f39bf9b98387d408f272a12550621
isabelberg <33488944+isabelberg@users.noreply.github.com>
2019-07-23 14:51:00 +0200

    Remove Id

--- src/models/EventType.ts
+++ src/models/EventType.ts
@@ -3,12 +3,10 @@ import { Schema, model, Document, Types } from 'mongoose';
 const { ObjectId } = Schema.Types.ObjectId;
 
 interface EventType extends Document {
-  eventId: Types.ObjectId;
   type: string;
 }
 
 const eventTypeSchema = new Schema({
-  eventId: ObjectId,
   type: { type: String, unique: true },
 });
 
