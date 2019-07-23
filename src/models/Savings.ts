a049d4948689e0c28ab7286295319795c6c65961
isabelberg <33488944+isabelberg@users.noreply.github.com>
2019-07-23 14:56:14 +0200

    Remove Id

--- src/models/Savings.ts
+++ src/models/Savings.ts
@@ -3,14 +3,12 @@ import { Schema, model, Document, Types } from 'mongoose';
 const { ObjectId } = Schema.Types.ObjectId;
 
 interface Savings extends Document {
-  savingsId: Types.ObjectId;
   name: string;
   goal: number;
   category: string;
 }
 
 const savingsSchema = new Schema({
-  savingsId: ObjectId,
   name: String,
   goal: Number,
   category: String,
