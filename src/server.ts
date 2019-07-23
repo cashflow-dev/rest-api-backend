9ef549652498ea7f96563b1256cfb1813adadf29
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-07-23 18:16:50 +0200

    Disable memwatch

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
+// TODO: See issue #19
+// import memwatch from 'node-memwatch';
 import cors from '@koa/cors';
 import fs from 'fs';
 import router from './routes/index';
@@ -13,6 +15,10 @@ const LOG_PATH = `${__dirname}/${process.env.LOG_PATH}`;
 if (!fs.existsSync(LOG_PATH)) {
   fs.closeSync(fs.openSync(LOG_PATH, 'w'));
 }
+
+// TODO: See issue #19
+// memwatch.on('leak', Logger.fatal);
+// memwatch.on('stats', Logger.fatal);
 
 const DATABASE = `${process.env.MONGODB_URI}`;
 
