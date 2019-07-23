0685592688c56a99ba94e7f11645217484ba1727
isabelberg <33488944+isabelberg@users.noreply.github.com>
2019-07-23 22:55:30 +0200

    Add password encryption

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import { Schema, model, Document } from 'mongoose';
+import { bcrypt } from 'bcrypt';
+
+const saltRounds = 10;
 
 interface User extends Document {
   email: string;
@@ -17,5 +20,33 @@ const userSchema = new Schema({
     origin: String,
   },
 });
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(error, hash) {
+      if (error) return next(error);
+
+      user.password = hash;
+      next();
+      return null;
+    });
+    return null;
+  });
+  return null;
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+    return null;
+  });
+};
 
 export default model<User>('User', userSchema);
