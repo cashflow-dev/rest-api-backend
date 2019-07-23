d8a41184f506575e6c8705742f592b4b9e70219f
isabelberg <33488944+isabelberg@users.noreply.github.com>
2019-07-23 14:52:39 +0200

    Remove Id

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -3,14 +3,12 @@ import { Schema, model, Document, Types } from 'mongoose';
 const { ObjectId } = Schema.Types.ObjectId;
 
 interface Budget extends Document {
-  budgetId: Types.ObjectId;
   name: string;
   amount: number;
   period: string;
 }
 
 const budgetSchema = new Schema({
-  budgetId: ObjectId,
   name: String,
   amount: Number,
   period: String,
