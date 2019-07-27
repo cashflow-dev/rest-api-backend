40834168d8283fb9a92fcefcd24b6229b76c50a3
Unknown <johan.gudmundsson2012@gmail.com>
2019-07-27 15:53:39 +0200

    Configure Jest setup & teardown

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,17 @@ module.exports = {
   roots: ['<rootDir>/src'],
   bail: 1,
   cache: true,
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
   testEnvironment: 'node',
   preset: 'ts-jest',
   cacheDirectory: '/tmp',
   collectCoverageFrom: ['./src/**/*.ts'],
   displayName: 'api-server',
-  setupFiles: ['./jest.setup.js'],
+  setupFiles: ['./jest.setup.ts'],
+  // globalSetup: './jest.init.ts',
+  globalTeardown: './jest.teardown.ts',
 };
---
+++ jest.setup.ts
@@ -0,0 +1,45 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import Logger from './src/libs/Logger';
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+const globalAny: any = global;
+
+let mongoServer: any;
+jest.setTimeout(30000);
+
+const setup = async () => {
+  Logger.debug('Test setup runs...');
+  try {
+    mongoServer = new MongoMemoryServer();
+    globalAny.mongoServer = mongoServer as any;
+    const uri = await mongoServer.getConnectionString();
+    const mongooseOpts = {
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    };
+
+    mongoose.connect(uri, mongooseOpts);
+    mongoose.connection.on('error', (e: any) => {
+      if (e.message.code === 'ETIMEDOUT') {
+        Logger.debug(e);
+        mongoose.connect(uri, mongooseOpts);
+      }
+      Logger.debug(e);
+    });
+    mongoose.connection.once('open', () => {
+      Logger.debug('connecting');
+      Logger.debug(`MongoDB successfully connected to ${uri}`);
+    });
+  } catch (e) {
+    Logger.debug(e);
+  }
+  Logger.debug('Test setup finished!');
+};
+
+(async () => {
+  await setup();
+})();
